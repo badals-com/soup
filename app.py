d2d0b067c438a61baf056a238e959ed53ff75099
Ali <ali@badals.com>
2023-11-20 21:34:26 -0500

    first commit

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     # URL of the product page
     url = 'https://www.macys.com/shop/product/michael-michael-kors-womens-single-breasted-wool-blend-coat-created-for-macys?ID=16050718&swatchColor=Crew%20Blue%20Blue'
+    url = request.args.get('url')
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
     }
